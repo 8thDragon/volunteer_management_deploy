96bdfd9ae993917cf0431611a8dac03b8bc0194a
8thDragon <panthorn.ch@gmail.com>
2023-06-14 01:03:07 +0700

    orgin fix 1

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   app.use('/public', express.static(join(__dirname, '../..', 'uploadedFiles/files')));
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:4200/',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept'
   })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -610,7 +610,7 @@ export class UsersService
     });
     const jwt = await this.jwtService.signAsync({ id: user.id });
     console.log(jwt);
-    response.cookie('jwt', jwt, { httpOnly: false });
+    response.cookie('jwt', jwt, { httpOnly: false, secure: true });
 
     if (!user) {
       responseC.error_code = '400';
